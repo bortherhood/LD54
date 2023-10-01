7554f7a2e019c6e9656fbfad357637c9753ba3ac
Mantarri <47439660+Mantarri@users.noreply.github.com>
2023-10-01 13:23:52 -0700

    Add buttons and some functionality to Main Menu, UI theme file

---
+++ Scenes/Menus/MainMenu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+onready var center_box: CenterContainer = $CenterBox
+onready var highscore_display: Label = $CenterBox/VBox/HighscoreBG/VBox/HighscoreDisplay
+
+func _ready():
+	center_box.set_size(Vector2(OS.window_size.x / 2, rect_size.y))
+	highscore_display.text = str(Settings.setting["highscore"])
